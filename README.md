36fa7b4401915a362a21237c36917b1d461c8aa9
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-09-11 00:48:42 +0700

    add migration db

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Docker-Spring-Cloud-Gateway
+
+This project was created to learn reactive programming, functional programming and custom api gateway with Spring Cloud Gateway
+
+## How To Run
+
+1. Run docker compose with command `docker-compose up`
+2. Run application with command `mvn clean spring-boot:run -Pdev`
+
+## How To Build
+
+1. Run build jar with command `mvn clean install -Pdev`
+2. Run build docker image with command `mvn clean docker:build -Pdev`
+
+## Feature
+
+- [x] Dynamic Routing
+- [x] Custom Filter
+- [x] Logging Request and Response
+- [x] Database Migration
+- [x] Docker Support
+- [x] View api route and applications credential
+- [ ] Create api route and applications credential
+- [ ] Update api route and applications credential
+- [ ] Delete api route and applications credential
+- [ ] User Management
+- [ ] Cache To Redis
+- [ ] Send Log to ELK
--- src/main/resources/db/migration/V0.0.2.20220911__initial_data.sql
+++ src/main/resources/db/migration/V0.0.2.20220911__initial_data.sql
@@ -1,32 +1,3 @@
-CREATE TABLE tb_application_credential
-(
-    id               varchar(36) PRIMARY KEY,
-    application_name varchar(100),
-    api_key          varchar(36)
-);
-
-CREATE TABLE tb_api_route
-(
-    id               varchar(36) PRIMARY KEY,
-    name             varchar(255),
-    version          varchar(10),
-    path             varchar(255),
-    rewrite_frontend varchar(255),
-    rewrite_backend  varchar(255),
-    method           varchar(10),
-    uri              varchar(255),
-    authentication   varchar(50)
-);
-
-CREATE TABLE tb_api_route_application_credential
-(
-    id                        varchar(36) PRIMARY KEY,
-    application_credential_id varchar(36),
-    api_route_id              varchar(36),
-    CONSTRAINT FK_Credential FOREIGN KEY (application_credential_id) REFERENCES tb_application_credential (id),
-    CONSTRAINT FK_Route FOREIGN KEY (api_route_id) REFERENCES tb_api_route (id)
-);
-
 INSERT INTO tb_api_route(id, name, version, path, rewrite_frontend, rewrite_backend, method, uri, authentication)
 values ('4c4239a3-9cca-441b-829a-178c2dda530a', 'http bin', '1.0.0', '/v1/**', '/v1/(?<segment>.*)', '/${segment}',
         'GET',
